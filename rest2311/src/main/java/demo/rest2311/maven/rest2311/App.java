4855489faf3da83b2ce61c5050dc567eed26a025
srilekha1818 <srilekhakavuri18@gmail.com>
2023-11-23 18:13:00 +0530

    commit p6

---
+++ rest2311/src/main/java/demo/rest2311/maven/rest2311/App.java
@@ -0,0 +1,13 @@
+package demo.rest2311.maven.rest2311;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
---
+++ rest2311/src/test/java/demo/rest2311/maven/rest2311/AppTest.java
@@ -0,0 +1,20 @@
+package demo.rest2311.maven.rest2311;
+
+import static org.junit.Assert.assertTrue;
+
+import org.junit.Test;
+
+/**
+ * Unit test for simple App.
+ */
+public class AppTest 
+{
+    /**
+     * Rigorous Test :-)
+     */
+    @Test
+    public void shouldAnswerWithTrue()
+    {
+        assertTrue( true );
+    }
+}
